cc1e25acfe6b05531777b3f4887d9c7b138eb8ad
programmerkenzi <68736908+programmerkenzi@users.noreply.github.com>
2023-03-01 23:20:16 +0800

    added folders ,files and pkgs

---
+++ internal/server/server.go
@@ -0,0 +1,47 @@
+package server
+
+import (
+	"flag"
+	"os"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/template/html"
+	"github.com/gofiber/websocket/v2"
+)
+
+var (
+	addr = flag.String("addr, ", ":", os.Getenv("PORT"))
+	cert = flag.String("cert, ", "", "")
+	key  = flag.String("key, ", "", "")
+)
+
+func Run() error {
+	flag.Parse()
+
+	if *addr == ":" {
+		*addr = ":8080"
+	}
+
+	engine := html.New("./views", ".html")
+	app := fiber.New(fiber.Config{Views: engine})
+	app.Use(logger.New())
+	app.Use(cors.New())
+
+	app.Get("/", handlers.Welcome)
+	app.Get("/room/create", handlers.RoomCreate)
+	app.Get("/room/:uuid", handlers.Room)
+	app.Get("/room/:uuid/websocket", websocket.New(handler.RoomWebsocket, websocket.config{
+		HandshakeTimeout: 20 * time.Second,
+	}))
+	app.Get("/room/:uuid/chat", handlers.RoomChat)
+	app.Get("/room/:uuid/chat/websocket", websocket.New(handlers.RoomChatWebsocket))
+	app.Get("/room/:uuid/viewer/websocket", websocket.New(handlers.RoomViewerWebsocket))
+	app.Get("/stream/:ssuid", handlers.Stream)
+	app.Get("/stream/:ssuid/websocket")
+	app.Get("/stream/:ssuid/chat/websocket")
+	app.Get("/stream/:ssuid/viewer/websocket")
+
+}
